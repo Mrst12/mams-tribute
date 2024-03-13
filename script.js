587bf1f5fcb3b1ac989bdd20c6ac6b1b429a9bac
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2024-03-13 16:24:57 +0000

    add images and styling for the image carousel

--- script.js
+++ script.js
@@ -2,6 +2,39 @@ const showNav = document.querySelector(".show-nav");
 const closeNav = document.querySelector(".close-nav");
 const nav = document.querySelector(".nav");
 
+currentSlideId = 1;
+sliderElement = document.getElementById("slider");
+totalSlides = sliderElement.childElementCount;
+
+function next() {
+    if (currentSlideId < totalSlides) {
+        currentSlideId ++;
+        showSlide()
+        
+    }
+        
+}
+
+function prev() {
+    if (currentSlideId > 1) {
+        currentSlideId--;
+        showSlide()
+    }
+        
+}
+
+function showSlide(){
+    slides = document.getElementById("slider").getElementsByTagName("li");
+    for (let index = 0; index < totalSlides; index++) {
+        const element = slides[index];
+        if (currentSlideId === index+1){
+            element.classList.remove("hidden")
+        }else{
+            element.classList.add("hidden")
+        }
+    }
+}
+
 closeNav.addEventListener("click", () => {
     nav.classList.remove("right-0")
     nav.classList.add("right-[-50%]");
@@ -14,3 +47,4 @@ showNav.addEventListener("click", () => {
     nav.classList.remove("bg-background")
     
 })
+
