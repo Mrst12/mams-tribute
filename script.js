81961f0177f3eee4f71617a4b749f9bac1ec4353
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2024-03-18 13:39:59 +0000

    change the structure of the images so the buttons worked better

--- script.js
+++ script.js
@@ -2,37 +2,36 @@ const showNav = document.querySelector(".show-nav");
 const closeNav = document.querySelector(".close-nav");
 const nav = document.querySelector(".nav");
 
-currentSlideId = 1;
-sliderElement = document.getElementById("slider");
-totalSlides = sliderElement.childElementCount;
+let images = [
+    "https://res.cloudinary.com/mrst12/image/upload/v1710335798/mams%20tribute/mam6_rzujcb.jpg",
+    "https://res.cloudinary.com/mrst12/image/upload/v1710335798/mams%20tribute/mam7_jmt4zk.jpg",
+    "https://res.cloudinary.com/mrst12/image/upload/v1710335797/mams%20tribute/mam8_ycuoat.jpg",
+    "https://res.cloudinary.com/mrst12/image/upload/v1710338085/mams%20tribute/mam9new_nvvdj1.jpg",
+    "https://res.cloudinary.com/mrst12/image/upload/v1710335795/mams%20tribute/mam_bo0jy2.jpg",
+    "https://res.cloudinary.com/mrst12/image/upload/c_scale,h_700,w_500/v1710335794/mams%20tribute/mam3_jmn60l.jpg",
+    "https://res.cloudinary.com/mrst12/image/upload/c_scale,w_600/v1710335794/mams%20tribute/mam2_p9wvbw.jpg",
+    "https://res.cloudinary.com/mrst12/image/upload/v1710335795/mams%20tribute/mam1_dfmg1o.jpg",
+    "https://res.cloudinary.com/mrst12/image/upload/c_scale,w_600/v1710335794/mams%20tribute/mam4_htvgbn.jpg",
+    "https://res.cloudinary.com/mrst12/image/upload/v1710335794/mams%20tribute/mam5_kipmff.jpg"
+]
+let num = 0;
 
 function next() {
-    if (currentSlideId < totalSlides) {
-        currentSlideId ++;
-        showSlide()
-        
+    let slider = document.getElementById("slider");
+    num ++;
+    if (num >= images.length) {
+        num = 0;
     }
-        
+    slider.src = images[num];
 }
 
 function prev() {
-    if (currentSlideId > 1) {
-        currentSlideId--;
-        showSlide()
+    let slider = document.getElementById("slider");
+    num --;
+    if (num < 0) {
+        num = images.length-1;
     }
-        
-}
-
-function showSlide(){
-    slides = document.getElementById("slider").getElementsByTagName("li");
-    for (let index = 0; index < totalSlides; index++) {
-        const element = slides[index];
-        if (currentSlideId === index+1){
-            element.classList.remove("hidden")
-        }else{
-            element.classList.add("hidden")
-        }
-    }
+    slider.src = images[num];
 }
 
 closeNav.addEventListener("click", () => {
