05296297b39bf34a355569059a58c873bde521cd
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2024-03-20 12:28:16 +0000

    add images into project as third party cookies depreciating

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let images = [
-    "https://res.cloudinary.com/mrst12/image/upload/v1710335798/mams%20tribute/mam6_rzujcb.jpg",
-    "https://res.cloudinary.com/mrst12/image/upload/v1710335798/mams%20tribute/mam7_jmt4zk.jpg",
-    "https://res.cloudinary.com/mrst12/image/upload/v1710335797/mams%20tribute/mam8_ycuoat.jpg",
-    "https://res.cloudinary.com/mrst12/image/upload/v1710338085/mams%20tribute/mam9new_nvvdj1.jpg",
-    "https://res.cloudinary.com/mrst12/image/upload/v1710335795/mams%20tribute/mam_bo0jy2.jpg",
-    "https://res.cloudinary.com/mrst12/image/upload/c_scale,h_700,w_500/v1710335794/mams%20tribute/mam3_jmn60l.jpg",
-    "https://res.cloudinary.com/mrst12/image/upload/c_scale,w_600/v1710335794/mams%20tribute/mam2_p9wvbw.jpg",
-    "https://res.cloudinary.com/mrst12/image/upload/v1710335795/mams%20tribute/mam1_dfmg1o.jpg",
-    "https://res.cloudinary.com/mrst12/image/upload/c_scale,w_600/v1710335794/mams%20tribute/mam4_htvgbn.jpg",
-    "https://res.cloudinary.com/mrst12/image/upload/v1710335794/mams%20tribute/mam5_kipmff.jpg"
+    "./assets/images/mam1_dfmg1o.jpg",
+    "./assets/images/mam2_p9wvbw.jpg",
+    "./assets/images/mam3_jmn60l.jpg",
+    "./assets/images/mam4_htvgbn.jpg",
+    "./assets/images/mam5_kipmff.jpg",
+    "./assets/images/mam6_rzujcb.jpg",
+    "./assets/images/mam7_jmt4zk.jpg",
+    "./assets/images/mam8_ycuoat.jpg",
+    "./assets/images/mam9new_nvvdj1.jpg",
+    "./assets/images/mam_bo0jy2.jpg"
 ]
 let num = 0;
 
@@ -28,6 +28,24 @@ function prev() {
         num = images.length-1;
     }
     slider.src = images[num];
+}
+
+
+// back to top button
+const toTopButton = document.getElementById("to-top-button");
+
+//show the button
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        toTopButton.classList.remove("hidden");
+    } else {
+        toTopButton.classList.add("hidden");
+    }
+}
+
+//scroll to top
+function goToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 
